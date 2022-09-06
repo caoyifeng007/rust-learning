7111cf9f3f4cd08c953a378d14c4baf682057b4b
ElvinsJ <caoyifeng007@163.com>
2022-09-06 21:13:07 +0800

    Start learning rust

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+
+    let mut v1;
+
+    v1 = 12;
+    println!("The variable before is: {}", v1);
+
+    v1 = 10;
+    println!("The variable after is: {}", v1);
+}
