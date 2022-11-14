4518399f93b45a80f4f1342269593c7a3474627d
ElvinsJ <caoyifeng007@163.com>
2022-11-14 23:07:14 +0800

    The scopes of the immutable references

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,29 @@ use std::io;
 //         .expect("Failed to read line");
 // }
 
+// fn main() {
+//     let mut counter = 0;
+
+//     let result = loop {
+//         counter += 1;
+
+//         if counter == 10 {
+//             break counter * 2;
+//         }
+//     };
+
+//     println!("The result is {result}");
+// }
+
 fn main() {
-    let mut counter = 0;
+    let mut s = String::from("hello");
 
-    let result = loop {
-        counter += 1;
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+    println!("{} and {}", r1, r2);
 
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-
-    println!("The result is {result}");
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
