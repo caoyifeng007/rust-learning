dba87c5cc02d146dccd363aef25826e53ddf6fb6
ElvinsJ <caoyifeng007@163.com>
2022-11-17 20:19:58 +0800

    Matching pattern

--- src/main.rs
+++ src/main.rs
@@ -26,15 +26,46 @@ use std::io;
 //     println!("The result is {result}");
 // }
 
+// fn main() {
+//     let mut s = String::from("hello");
+
+//     let r1 = &s; // no problem
+//     let r2 = &s; // no problem
+//     println!("{} and {}", r1, r2);
+//     // variables r1 and r2 will not be used after this point
+//     println!("{} and {}", r1, r2);
+
+//     let r3 = &mut s; // no problem
+//     println!("{}", r3);
+// }
+
+// fn main() {
+//     let mut s = String::from("Hello world");
+//     let s2 = &mut s;
+//     let s3 = s2;
+//     println!("{}", s2); // Not valid because s2 is moved
+// }
+
+fn decr_twice_v1(n: u32) -> Option<u32> {
+    match n {
+        0 => None,
+        1 => None,
+        n2 => {
+            println!("{}", n2);
+            Some(n2 - 2)
+        }
+    }
+}
+
+fn decr_twice_v2(n: u32) -> Option<u32> {
+    if n == 0 {
+        None
+    } else if n == 1 {
+        None
+    } else {
+        Some(n - 2)
+    }
+}
 fn main() {
-    let mut s = String::from("hello");
-
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{} and {}", r1, r2);
-    // variables r1 and r2 will not be used after this point
-    println!("{} and {}", r1, r2);
-
-    let r3 = &mut s; // no problem
-    println!("{}", r3);
+    let out = decr_twice_v1(2);
 }
